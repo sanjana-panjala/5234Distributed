9ac0b5022827f533ed8324922c19d34cd97d8ab7
Ryan <ryan.bax.17@gmail.com>
2022-10-02 22:18:39 -0400

    Addition of confirmation button and routing to viewOrder.js

--- src/components/viewOrder.js
+++ src/components/viewOrder.js
@@ -1,24 +1,27 @@
 import React from "react";
-import {useLocation} from "react-router-dom"
+import {useNavigate, useLocation} from "react-router-dom";
 
 const viewOrder = () => {
 
-const location = useLocation();
-    console.log(location)
-    return <div> {location.state.order.buyQuantity[2]} </div>
+    const location = useLocation();
+    console.log(location);
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        navigate('/purchase/viewConfirmation')
+    };
 
-/*
     return (
         <div>
+            <form onSubmit={handleSubmit}>
             <h2>
                 Order Details
             </h2>
-            <ul style="list-style-type:none;">
+            <ul>
             </ul>
             <h2>
                 Payment Information
             </h2>
-            <ul style="list-style-type:none;">
+            <ul>
                 <li>Card Holder Name: {location.state.order.card_holder_name}</li>
                 <li>Credit Card Number: {location.state.order.credit_card_number}</li>
                 <li>Expriation Date: {location.state.order.expir_date}</li>
@@ -27,16 +30,19 @@ const location = useLocation();
             <h2>
                 Shipping Details
             </h2>
-            <ul style="list-style-type:none;">
+            <ul>
                 <li>Address 1: {location.state.order.address_1}</li>
                 <li>Address 2: {location.state.order.address_2}</li>
                 <li>City: {location.state.order.city}</li>
                 <li>State: {location.state.order.state}</li>
                 <li>Zip Code: {location.state.order.zip}</li>
             </ul>
+            <button className="button">Confirm</button>
+            </form>
         </div>
-    )
-*/
+
+    );
+
 }
 
 export default viewOrder;
