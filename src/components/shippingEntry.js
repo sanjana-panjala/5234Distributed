dd3181695796d2ee57b2f801ae92415be24ccbfc
Mohith Kankanala <kankanala.3@osu.edu>
2022-10-02 20:23:31 -0400

    added email and shipping type fields

--- src/components/shippingEntry.js
+++ src/components/shippingEntry.js
@@ -6,11 +6,9 @@ const shippingEntry = () => {
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
-        navigate('/purchase/viewOrder', {state:{order: location.state.order}}
+        navigate('/purchase/paymentEntry', {state:{order: location.state.order}}
         )
     };
-
-
 
     return (
         <div>
@@ -28,31 +26,50 @@ const shippingEntry = () => {
                         (e) => {{location.state.order.address_1 = e.target.value;}}
                     }
                 />
+                <br />
                 <label>Address 2:</label>
                 <input
                     type="text"
                     required
                     onChange={(e) => {{location.state.order.address_2 = e.target.value;}}}
                 />
+                <br />
                 <label>City:</label>
                 <input
                     type="text"
                     required
                     onChange={(e) => {{location.state.order.city = e.target.value;}}}
                 />
+                <br />
                 <label>State:</label>
                 <input
                     type="text"
                     required
                     onChange={(e) => {{location.state.order.state = e.target.value;}}}
                 />
+                <br />
                 <label>Zip:</label>
                 <input
                     type="text"
                     required
                     onChange={(e) => {{location.state.order.zip = e.target.value;}}}
                 />
+                <br />
+                <label>Confirm Email:</label>
+                <input
+                    type="text"
+                    required
 
+                />
+                <br />
+                <label>Expedited</label>
+                <input
+                    type="checkbox"
+                    required
+                    onChange={(e) => {{location.state.order.expedited = true}}}
+
+                />
+                <br />
                 <button className="button">View Order</button>
 
 
