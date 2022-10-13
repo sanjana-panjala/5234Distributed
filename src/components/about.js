f10930a1cca04300ed2d517a565887df1ca06469
ryanbax17 <ryan.bax.17@gmail.com>
2022-10-12 22:27:55 -0400

    Addition of About us page with styling that I 'borrowed' from a generous website.

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,48 @@
 import React from "react";
 
 const About = () => {
-  
+
   return (
-    <div>
-      <h1>
-        about page
-      </h1>
+    <div class="bg-light py-5">
+      <div class="container py-5">
+        <div class="row mb-4">
+          <div class="col-lg-5">
+            <h2 class="display-4 font-weight-light">Our team</h2>
+            <p class="font-italic text-muted">Meet the people who make everything happen.</p>
+          </div>
+        </div>
+
+        <div class="row text-center">
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
-  
+
 export default About;
+
+/* https://bootstrapious.com/p/about-us-page */
